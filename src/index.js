0dd27743aaee8c0055a16ef8156fc14306f7565e
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-10-02 11:58:00 +0200

    remove test console.logs

--- src/index.js
+++ src/index.js
@@ -3,10 +3,4 @@ import Project from "./modules/project";
 
 const test = new Project()
 
-test.createTodo("test")
 
-console.log(test.todoList)
-test.createTodo("test2")
-console.log(test.todoList)
-test.todoList[0].setNotes("test")
-console.log(test.todoList[0])
